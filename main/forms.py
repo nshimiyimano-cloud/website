58632ef4d1cb3985201931dd737dfdef313efec7
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-02-21 10:25:55 +0200

    howto add login_reqired decorator to enforce login

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
     email =forms.EmailField(required=True)
     first_name=forms.CharField(required=True)
+    
     class Meta:
         model = User 
         fields = ["username","email","first_name","password1","password2"]
